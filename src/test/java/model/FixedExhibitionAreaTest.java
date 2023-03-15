cd641bb00870756785ff2b63774b7761635dc47f
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-03-15 11:48:47 +0100

    Added JUnit dependency

---
+++ src/test/java/model/FixedExhibitionAreaTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FixedExhibitionAreaTest {
+
+    @org.junit.jupiter.api.Test
+    void occupy() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isOccupied() {
+    }
+}
