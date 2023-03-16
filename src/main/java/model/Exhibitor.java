926708872bc29f5b7f1ae9f6fbcab82ebad0f679
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-03-16 12:45:18 +0100

    Refactoring + modified getExhibitionList method

--- src/main/java/model/Exhibitor.java
+++ src/main/java/model/Exhibitor.java
@@ -1,5 +1,8 @@
 package model;
 
+import jdbc.ExhibitionDataAccess;
+import jdbc.ExhibitorDataAccess;
+
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,17 +27,28 @@ public class Exhibitor {
         return exhibitorName;
     }
 
-    public List<Exhibition> getExhibitionList() {
+    public List<Exhibition> getExhibitionList() throws SQLException {
+        if (exhibitionList.isEmpty()) {
+            exhibitionList = new ExhibitorDataAccess().getExhibitionsByExhibitorId(this.getExhibitorId());
+        }
         return exhibitionList;
     }
 
     public boolean bookExhibitionArea(Expo expo, LocalDateTime start, LocalDateTime end) throws SQLException {
         Exhibition exhibition = expo.concedeExhibitionArea(this, start, end);
-        if(exhibition != null) {
+        if (exhibition != null) {
             exhibitionList.add(exhibition);
             return true;
         } else {
             return false;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Exhibitor{" +
+                "exhibitorId=" + exhibitorId +
+                ", exhibitorName='" + exhibitorName + '\'' +
+                '}';
     }
 }
