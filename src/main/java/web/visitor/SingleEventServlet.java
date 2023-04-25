24960a124f0c4d0bdc5060bac9d8d514e3caf574
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-04-25 18:01:26 +0200

    This one is for the purchase functionality

---
+++ src/main/java/web/visitor/SingleEventServlet.java
@@ -0,0 +1,33 @@
+package web.visitor;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Event;
+import service.EventService;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class SingleEventServlet
+ */
+public class SingleEventServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Integer eventId = Integer.valueOf(request.getParameter("eventId")); // TODO surround with try/catch?
+		Event event = new EventService().findEvent(eventId);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("event", event);
+		
+		request.getRequestDispatcher("/single_event.jsp").forward(request, response);
+	}
+
+}
