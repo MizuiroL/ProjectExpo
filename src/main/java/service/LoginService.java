00c3b7edce7d7cf80adb69053f03798e6c5a0507
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-04-27 10:50:22 +0200

    Added session attributes

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -3,16 +3,20 @@ package service;
 import exceptions.UserNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jdbc.ExpoDAO;
 import jdbc.LoginDAO;
+import model.ExpoManager;
 import model.Visitor;
 
 public class LoginService {
 	public static HttpSession visitorLogin(String email, String password, HttpServletRequest request) throws UserNotFoundException {
-		System.out.println("LoginService: email " + email + " password " + password);
-		Visitor visitor = new LoginDAO().login(email, password);
+		//System.out.println("LoginService: email " + email + " password " + password);
+		Visitor visitor = new LoginDAO().login(email, password); // Throws UserNotFoundException
+		ExpoManager expo = new ExpoDAO().getExpoById(1);
 		HttpSession session = request.getSession();
 		session.setAttribute("visitor", visitor);
-		session.setAttribute("loggedIn", Boolean.FALSE);
+		session.setAttribute("expo", expo);
+		//session.setAttribute("loggedIn", Boolean.FALSE);
 		return session;
 	}
 }
