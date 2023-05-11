d344338832269fd8dc8df0ab056f4ba7a9e9c6a3
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-05-11 12:47:33 +0200

    Create EventAlreadyBookedException.java

---
+++ src/main/java/exceptions/EventAlreadyBookedException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class EventAlreadyBookedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EventAlreadyBookedException(String e) {
+		super(e);
+	}
+
+}
